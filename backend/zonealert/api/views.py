93f71d88d506e2173b62326b18123f980cdeb210
derek-mo <derek.mo7203@gmail.com>
2024-10-27 13:30:31 -0400

    working with NOAA but getting issues

--- backend/zonealert/api/views.py
+++ backend/zonealert/api/views.py
@@ -4,6 +4,19 @@ from rest_framework.response import Response
 from ibm_watsonx_ai.foundation_models import ModelInference
 from ibm_watsonx_ai import APIClient, Credentials
 from django.conf import settings
+from noaa_sdk import NOAA
+import json
+
+n = NOAA()
+observations = n.get_observations('32607', 'US')
+weather_info = {
+    "temperature": observations['temperature']['value'],
+    "wind_speed": observations['windSpeed']['value'],
+    "wind_direction": observations['windDirection']['value'],
+    "cloud_coverage": observations['textDescription'],
+    "visibility": observations['visibility']['value'],
+    "humidity": observations['relativeHumidity']['value']
+}
 
 credentials = Credentials(
     url = "https://us-south.ml.cloud.ibm.com",
@@ -19,7 +32,21 @@ model = ModelInference(
   params={"max_new_tokens": 100}
 )
 
-prompt = "What is the weather like in San Francisco?"
+prompt = {
+    "text": "Watson, based on the current weather conditions, what are the safest shelters in Gainesville, Florida?",
+    "entities": {
+        "location": {
+            "value": "Gainesville, Florida"
+        },
+        "weather": {
+            "value": json.dumps(weather_info)  # Include all observations as JSON
+        }
+    }
+}
+
+        # "shelters": {
+        #     "value": "list of safe shelters"
+        # },
 
 @api_view(['GET'])
 def map(request):
